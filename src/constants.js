a280c307e61c454e8f4567433e6e38d4ac596881
Stefan <stefan.braunovic@vegait.rs>
2022-08-16 15:04:31 +0200

    The Character Select page

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+const CONTRACT_ADDRESS = '0x30E961610660723889de27C8534bea9D92899F67'
+
+
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+
+export { CONTRACT_ADDRESS,transformCharacterData };
+
