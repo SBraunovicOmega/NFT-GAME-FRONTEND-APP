869f293e402833c41a13cf94ec48cefb66aab5cd
Stefan <stefan.braunovic@vegait.rs>
2022-08-16 14:09:32 +0200

    connect to wallet button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -7,31 +7,107 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header gradient-text">⚔️ Balkanverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Balkanverse!</p>
-          <div className="connect-wallet-container">
-            <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
-            />
-          </div>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!');
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+
+        /*
+         * Check if we're authorized to access the user's wallet
+         */
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        /*
+         * User can have multiple authorized accounts, we grab the first one if its there!
+         */
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log('Found an authorized account:', account);
+          setCurrentAccount(account);
+        } else {
+          console.log('No authorized account found');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+}
+
+const connectWalletAction = async () => {
+  try {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert('Get MetaMask!');
+      return;
+    }
+
+    /*
+     * Fancy method to request access to account.
+     */
+    const accounts = await ethereum.request({
+      method: 'eth_requestAccounts',
+    });
+
+    /*
+     * Boom! This should print out public address once we authorize Metamask.
+     */
+    console.log('Connected', accounts[0]);
+    setCurrentAccount(accounts[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+useEffect(()=>{
+ checkIfWalletIsConnected();
+},[])
+
+return (
+  <div className="App">
+    <div className="container">
+      <div className="header-container">
+        <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
+        <p className="sub-text">Team up to protect the Metaverse!</p>
+        <div className="connect-wallet-container">
+          
+          <img
+            src="https://balkan.do.am/balkanpower.gif"
+            alt="Monty Python Gif"
+          />
+          {/*
+           * Button that we will use to trigger wallet connect
+           * Don't forget to add the onClick event to call your method!
+           */}
+          <button
+            className="cta-button connect-wallet-button"
+            onClick={connectWalletAction}
+          >
+            Connect Wallet To Get Started
+          </button>
         </div>
       </div>
+      <div className="footer-container">
+        <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+        <a
+          className="footer-text"
+          href={TWITTER_LINK}
+          target="_blank"
+          rel="noreferrer"
+        >{`built with @${TWITTER_HANDLE}`}</a>
+      </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default App;
